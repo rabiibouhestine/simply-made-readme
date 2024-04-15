1936c8de32483f7ecf11404a39f31db9886d64d7
Rabii Bouhestine <rabiibouhestine@outlook.com>
2024-04-15 14:32:00 +0100

    landing page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,36 @@
 <script>
 	import '../app.css';
+
+	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
 </script>
 
+<svelte:head>
+	<script
+		data-name="BMC-Widget"
+		data-cfasync="false"
+		src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
+		data-id="rabiibouhestine"
+		data-description="Support me on Buy me a coffee!"
+		data-message=""
+		data-color="#FF813F"
+		data-position="Right"
+		data-x_margin="18"
+		data-y_margin="18"
+	></script>
+</svelte:head>
+
+<Navbar>
+	<NavBrand href="/">
+		<!-- <img src="/images/flowbite-svelte-icon-logo.svg" class="me-3 h-6 sm:h-9" alt="Flowbite Logo" /> -->
+		<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+			Simply Made Readme
+		</span>
+	</NavBrand>
+	<NavHamburger />
+	<NavUl>
+		<NavLi href="/">Home</NavLi>
+		<NavLi href="/about">About</NavLi>
+		<NavLi href="/docs/components/navbar">FAQ</NavLi>
+	</NavUl>
+</Navbar>
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,34 @@
-<h1 class="text-3xl font-bold underline">Hello world!</h1>
+<script>
+	import { Heading, P, Button } from 'flowbite-svelte';
+</script>
+
+<div class="flex flex-col gap-32 my-32 items-center">
+	<div class="flex flex-col items-center max-w-3xl mx-auto">
+		<Heading
+			tag="h1"
+			class="mb-4"
+			customSize="text-center text-4xl font-extrabold  md:text-5xl lg:text-6xl"
+		>
+			The easiest way to create a <span class="text-primary-600">README</span>
+		</Heading>
+		<P class="mb-6 text-center text-lg lg:text-xl px-16  dark:text-gray-400">
+			A simple editor that allows you to quickly add and customize all the sections you need for
+			your project's readme
+		</P>
+		<div class="flex flex-col gap-2 items-center">
+			<Button size="xl" href="/">Get Started</Button>
+			<p class="text-sm text-slate-600">No Email Required</p>
+		</div>
+	</div>
+	<img
+		src="https://readme.so/_next/image?url=%2Fscreenshot.png&w=3840&q=75"
+		alt="editor screenshot"
+		class="rounded-lg max-w-6xl mx-auto"
+	/>
+	<p class="flex gap-1">
+		Made with <span class="text-pink-400">♡</span> by
+		<a href="https://rabiibouhestine.com" target="_blank" class="text-primary-600 hover:underline">
+			Rabii Bouhestine
+		</a>
+	</p>
+</div>
