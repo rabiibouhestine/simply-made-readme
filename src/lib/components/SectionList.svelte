92f80568bf4e03324fc624604d523b19214478e9
Rabii Bouhestine <rabiibouhestine@outlook.com>
2024-04-16 10:25:38 +0100

    clean up

--- src/lib/components/SectionList.svelte
+++ src/lib/components/SectionList.svelte
@@ -7,7 +7,7 @@
 		{ id: 1, name: 'item1' },
 		{ id: 2, name: 'item2' },
 		{ id: 3, name: 'item3' },
-		{ id: 4, name: 'item4' }
+		{ id: 4, name: 'Attribution (Not Required)' }
 	];
 
 	const flipDurationMs = 300;
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 	import '../app.css';
 
 	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+	import { DarkMode } from 'flowbite-svelte';
 </script>
 
 <svelte:head>
@@ -27,10 +28,13 @@
 		</span>
 	</NavBrand>
 	<NavHamburger />
-	<NavUl>
+	<NavUl
+		ulClass="flex items-center flex-col p-4 mt-4 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:text-sm md:font-medium"
+	>
 		<NavLi href="/">Home</NavLi>
 		<NavLi href="/about">About</NavLi>
 		<NavLi href="/docs/components/navbar">FAQ</NavLi>
+		<DarkMode />
 	</NavUl>
 </Navbar>
 <slot />
