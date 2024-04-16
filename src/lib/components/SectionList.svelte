193179cd208f6d1179b1211fe05fbf8e842c5860
Rabii Bouhestine <rabiibouhestine@outlook.com>
2024-04-16 15:30:54 +0100

    implement drag handle

--- src/lib/components/SectionList.svelte
+++ src/lib/components/SectionList.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
 	import { flip } from 'svelte/animate';
-	import { dndzone } from 'svelte-dnd-action';
+	import { dndzone, SOURCES, TRIGGERS } from 'svelte-dnd-action';
 	import { TrashBinOutline, DotsVerticalOutline } from 'flowbite-svelte-icons';
+	import { Button } from 'flowbite-svelte';
 
+	const flipDurationMs = 200;
+	const dropTargetStyle = {};
+	let dragDisabled = true;
 	let items = [
 		{ id: 1, name: 'Title' },
 		{ id: 2, name: 'Description' },
@@ -10,38 +14,69 @@
 		{ id: 4, name: 'Attribution (Not Required)' }
 	];
 
-	const flipDurationMs = 300;
-	const dropTargetStyle = {};
-
-	function handleDndConsider(e: any) {
-		items = e.detail.items;
+	function handleConsider(e: any) {
+		const {
+			items: newItems,
+			info: { source, trigger }
+		} = e.detail;
+		items = newItems;
+		// Ensure dragging is stopped on drag finish via keyboard
+		if (source === SOURCES.KEYBOARD && trigger === TRIGGERS.DRAG_STOPPED) {
+			dragDisabled = true;
+		}
 	}
-	function handleDndFinalize(e: any) {
-		items = e.detail.items;
+	function handleFinalize(e: any) {
+		const {
+			items: newItems,
+			info: { source }
+		} = e.detail;
+		items = newItems;
+		// Ensure dragging is stopped on drag finish via pointer (mouse, touch)
+		if (source === SOURCES.POINTER) {
+			dragDisabled = true;
+		}
+	}
+	function startDrag(e: any) {
+		// preventing default to prevent lag on touch devices (because of the browser checking for screen scrolling)
+		e.preventDefault();
+		dragDisabled = false;
+	}
+	function handleKeyDown(e: any) {
+		if ((e.key === 'Enter' || e.key === ' ') && dragDisabled) dragDisabled = false;
 	}
 </script>
 
 <div
 	class="w-full"
-	use:dndzone={{ items, flipDurationMs, dropTargetStyle }}
-	on:consider={handleDndConsider}
-	on:finalize={handleDndFinalize}
+	use:dndzone={{ items, dragDisabled, flipDurationMs, dropTargetStyle }}
+	on:consider={handleConsider}
+	on:finalize={handleFinalize}
 >
 	{#each items as item (item.id)}
 		<div class="flex mb-2" animate:flip={{ duration: flipDurationMs }}>
-			<div class="bg-gray-50 p-4 border flex rounded-l-lg">
+			<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+			<!-- svelte-ignore a11y-no-static-element-interactions -->
+			<div
+				tabindex={dragDisabled ? 0 : -1}
+				aria-label="drag-handle"
+				style={dragDisabled ? 'cursor: grab' : 'cursor: grabbing'}
+				on:mousedown={startDrag}
+				on:touchstart={startDrag}
+				on:keydown={handleKeyDown}
+				class="handle bg-gray-50 p-4 border flex rounded-l-lg"
+			>
 				<DotsVerticalOutline class="w-6 h-6" />
 				<DotsVerticalOutline class="w-6 h-6 -ml-4" />
 				<DotsVerticalOutline class="w-6 h-6 -ml-4" />
 			</div>
-			<div class="bg-gray-50 p-4 border-t border-b grow">
+			<button class="bg-gray-50 p-4 border-t border-b grow text-left hover:text-primary-600">
 				{item.name}
-			</div>
-			<div
+			</button>
+			<button
 				class="bg-gray-50 text-gray-500 hover:bg-red-500 hover:text-gray-100 p-4 border rounded-r-lg"
 			>
 				<TrashBinOutline class="w-6 h-6 " />
-			</div>
+			</button>
 		</div>
 	{/each}
 </div>
