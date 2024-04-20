5800a20a464f7f05cedca4d6e734a1c9e657056c
Rabii Bouhestine <rabiibouhestine@outlook.com>
2024-04-20 13:08:38 +0100

    clean up

--- src/lib/sections.ts
+++ src/lib/sections.ts
@@ -69,7 +69,7 @@ const sections = [
       markdown: `
   ## Screenshots
   
-  ![App Screenshot](https://via.placeholder.com/468x300?text=App+Screenshot+Here)
+  ![App Screenshot](https://via.placeholder.com/600x300?text=App+Screenshot+Here)
   
   `,
     },
